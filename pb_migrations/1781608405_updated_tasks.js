ba73e06aad6189cd321bea5aa4cd3165ca79af4e
0xAceBit <lastdurec@gmail.com>
2026-06-16 13:02:21 +0100

    Initial PocketBase deployment

---
+++ pb_migrations/1781608405_updated_tasks.js
@@ -0,0 +1,150 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2602490748")
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text1843675174",
+    "max": 0,
+    "min": 0,
+    "name": "description",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(3, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text1322349139",
+    "max": 0,
+    "min": 0,
+    "name": "reward",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(4, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text105650625",
+    "max": 0,
+    "min": 0,
+    "name": "category",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(5, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text1587448267",
+    "max": 0,
+    "min": 0,
+    "name": "location",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text2063623452",
+    "max": 0,
+    "min": 0,
+    "name": "status",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text3154569827",
+    "max": 0,
+    "min": 0,
+    "name": "creator",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text4227416285",
+    "max": 0,
+    "min": 0,
+    "name": "proof",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2602490748")
+
+  // remove field
+  collection.fields.removeById("text1843675174")
+
+  // remove field
+  collection.fields.removeById("text1322349139")
+
+  // remove field
+  collection.fields.removeById("text105650625")
+
+  // remove field
+  collection.fields.removeById("text1587448267")
+
+  // remove field
+  collection.fields.removeById("text2063623452")
+
+  // remove field
+  collection.fields.removeById("text3154569827")
+
+  // remove field
+  collection.fields.removeById("text4227416285")
+
+  return app.save(collection)
+})
